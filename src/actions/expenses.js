5958ca37c7dbaf036d2caa0ceffbcd5288650aeb
Antonio <toni.cm@gmail.com>
2019-09-19 18:55:05 -0300

    Setup remove expenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }))
+    })
+  }
+}
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -50,17 +58,17 @@ export const startSetExpenses = () => {
   return (dispatch) => {
 
     return database.ref('expenses')
-    .once('value')
-    .then((snapshot) => {
+      .once('value')
+      .then((snapshot) => {
         const expenses = []
 
         snapshot.forEach((childSnapshot) => {
-            expenses.push({
-                id: childSnapshot.key,
-                ...childSnapshot.val()
-            })
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          })
         })
         dispatch(setExpenses(expenses))
-    })
+      })
   }
 }
